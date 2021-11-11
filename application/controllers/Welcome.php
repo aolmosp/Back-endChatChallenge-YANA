bc6e56cd45a2ba909b341928de86d4c999f3ba3d
Angel Olmos <angel.olmospinto@gmail.com>
2021-11-11 19:59:10 -0300

    Re-config codeigniter whit codeigniter-restserver

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+use chriskacerguis\RestServer\RestController;
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+class Welcome extends RestController {
+
+	function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+    }
+	
+	public function users_get()
 	{
-		$this->load->view('welcome_message');
+		$this->response( [
+			'status' => false,
+			'message' => 'No such user found'
+		], 404 );
 	}
 }
